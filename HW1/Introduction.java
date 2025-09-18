384e51befb76e5ef82d86d7395ceff19c04bf5e1
dinhtuanz <24021657@vnu.edu.vn>
2025-09-19 00:57:17 +0700

    Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+    public static void main(String[] args) {
+
+        System.out.println("Le Dinh Anh Tuan\t24021657\tCS5\tdinhtuanz\t24021657@vnu.edu.vn\n");
+        for(int i =9;i>=1;i--){
+
+            System.out.println(i+" bottles of beer on the wall, "+ i+ " bottles of beer.\nTake one down, pass it around," );
+        
+        }
+        System.out.println("No more bottles of beer on the wall.");
+                    
+
+    }
+}
