071d445cbdb435291e2b77c7b2262b0a51d6f073
dinhtuanz <24021657@vnu.edu.vn>
2025-09-22 23:06:02 +0700

    2 + .git commit README.md

---
+++ HW1/AllEqual.java
@@ -0,0 +1,11 @@
+public class AllEqual {
+    public static void main(String[] args) {
+        if (args[0].equals(args[1])&&args[2].equals(args[1])) {
+            System.out.println("equal");
+        }
+        else {
+            System.out.println("not equal");
+        }
+    }
+    
+}
