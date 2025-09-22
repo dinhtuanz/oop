0e9a75bc36415dd00fc57996a2c69792ee002cd5
dinhtuanz <24021657@vnu.edu.vn>
2025-09-23 03:14:54 +0700

    Fn HW1

---
+++ HW1/RandomWalk.java
@@ -0,0 +1,42 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import java.awt.Color;
+import java.util.Random;
+
+public class RandomWalk {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        StdDraw.setScale(-n - 0.5, n + 0.5);
+        StdDraw.clear(StdDraw.GRAY);
+        StdDraw.enableDoubleBuffering();
+
+        int x = 0, y = 0;
+        int steps = 0;
+        int f = 0;
+        while (Math.abs(x) <= n && Math.abs(y) <= n) {
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledSquare(x, y, 0.45);
+            
+            
+            if(steps == 0 ) {
+                 x++;
+            }
+            else if ( y == -f && x != -f) x--;
+            else if ( y == f && x != f ) x++;
+            else if ( x == f && y != -f ) y--;
+            else if ( x == -f && y != f) y++;
+            else x++;
+            if( Math.sqrt(steps) % 2 == 1) {
+                 f++;
+            }
+            steps++;
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.filledSquare(x, y, 0.45);
+            StdDraw.show();
+            StdDraw.pause(40);
+        }
+        StdOut.println("Total steps = " + steps);
+    }
+
+}
